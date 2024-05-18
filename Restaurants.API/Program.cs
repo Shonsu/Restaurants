9e4f6df00e5b1a04542bc2aa1734d2017ae4628e
Shonsu <shonsu82@gmail.com>
2024-05-18 20:10:10 +0200

    move presentation layer from program.cs to extension

--- Restaurants.API/Program.cs
+++ Restaurants.API/Program.cs
@@ -1,4 +1,4 @@
-using Microsoft.OpenApi.Models;
+using Restaurants.API.Extensions;
 using Restaurants.API.Middlewares;
 using Restaurants.Application.Extensions;
 using Restaurants.Domain.Entities;
@@ -10,48 +10,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.AddSecurityDefinition(
-        "bearerAuth",
-        new OpenApiSecurityScheme
-        {
-            // In = ParameterLocation.Header,
-            // Description = "Please enter token",
-            // Name = "Authorization",
-            Type = SecuritySchemeType.Http,
-            // BearerFormat = "JWT",
-            Scheme = "Bearer"
-        }
-    );
-    c.AddSecurityRequirement(
-        new OpenApiSecurityRequirement
-        {
-            {
-                new OpenApiSecurityScheme
-                {
-                    Reference = new OpenApiReference
-                    {
-                        Type = ReferenceType.SecurityScheme,
-                        Id = "bearerAuth"
-                    }
-                },
-                []
-            }
-        }
-    );
-});
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddScoped<ErrorHandlingMiddleware>();
-builder.Services.AddScoped<RequestTimeLoggingMiddleware>();
-
+builder.AddPresentation();
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
-builder.Host.UseSerilog(
-    (context, configuration) => configuration.ReadFrom.Configuration(context.Configuration)
-);
 
 var app = builder.Build();
 var scope = app.Services.CreateScope();

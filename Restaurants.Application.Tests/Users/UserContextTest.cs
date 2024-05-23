caab82bc289afd57a355dc91cd30294d35039c64
Shonsu <shonsu82@gmail.com>
2024-05-23 10:44:27 +0200

    add user context tests

---
+++ Restaurants.Application.Tests/Users/UserContextTest.cs
@@ -0,0 +1,62 @@
+﻿using System.Security.Claims;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using Restaurants.Application.Users;
+using Restaurants.Domain.Constans;
+
+namespace Restaurants.Application.Tests;
+
+public class UserContextTest
+{
+    [Fact]
+    public void GetCurrentUser_WithAuthenticatedUser_ShouldReturnCurrentUser()
+    {
+        var dateOfBirth = new DateOnly(1990, 1, 1);
+        // arrange
+        var httpContextAccessorMock = new Mock<IHttpContextAccessor>();
+        var claims = new List<Claim>()
+        {
+            new(ClaimTypes.NameIdentifier, "1"),
+            new(ClaimTypes.Email, "test@test.com"),
+            new(ClaimTypes.Role, UserRoles.Admin),
+            new(ClaimTypes.Role, UserRoles.User),
+            new("Nationality", "German"),
+            new("DateOfBirth", dateOfBirth.ToString("yyyy-MM-dd")),
+        };
+        var user = new ClaimsPrincipal(new ClaimsIdentity(claims, "TEST"));
+        httpContextAccessorMock
+            .Setup(s => s.HttpContext)
+            .Returns(new DefaultHttpContext() { User = user });
+        var userContext = new UserContext(httpContextAccessorMock.Object);
+
+        // act
+        var currentUser = userContext.GetCurrentUser();
+
+        // assert
+        currentUser.Should().NotBeNull();
+        currentUser.Id.Should().Be("1");
+        currentUser.Email.Should().Be("test@test.com");
+        currentUser.Roles.Should().ContainInOrder(UserRoles.Admin, UserRoles.User);
+        currentUser.Nationality.Should().Be("German");
+        currentUser.DateOfBirth.Should().Be(dateOfBirth);
+    }
+
+    [Fact]
+    public void GetCurrentUser_WithUserContextNotPresent_ThrowInvalidOperationException()
+    {
+        // arrange
+        var httpContextAccessorMock = new Mock<IHttpContextAccessor>();
+        httpContextAccessorMock.Setup(ca => ca.HttpContext).Returns((HttpContext)null);
+        var userContext = new UserContext(httpContextAccessorMock.Object);
+
+        //act
+        Action action = () => userContext.GetCurrentUser();
+
+        // assert
+        action
+            .Should()
+            .Throw<InvalidOperationException>()
+            .WithMessage("User context is not preset");
+    }
+}

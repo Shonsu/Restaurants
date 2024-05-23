7f1cb97ebd92f48a30e952db9ec1ca8d441f049d
Shonsu <shonsu82@gmail.com>
2024-05-23 22:10:12 +0200

    refactor tests for update restaurant command handler

--- Restaurants.Application.Tests/Restaurants/Commands/UpdateRestaurant/UpdateRestaurantCommandHandlerTests.cs
+++ Restaurants.Application.Tests/Restaurants/Commands/UpdateRestaurant/UpdateRestaurantCommandHandlerTests.cs
@@ -2,7 +2,6 @@
 using FluentAssertions;
 using Microsoft.Extensions.Logging;
 using Moq;
-using Restaurants.Application.Restaurants.Dtos;
 using Restaurants.Domain.Constans;
 using Restaurants.Domain.Entities;
 using Restaurants.Domain.Exceptions;
@@ -13,119 +12,107 @@ namespace Restaurants.Application.Restaurants.Commands.UpdateRestaurant.Tests;
 
 public class UpdateRestaurantCommandHandlerTests
 {
- 
+    private readonly Mock<ILogger<UpdateRestaurantCommandHandler>> _loggerMock;
+    private readonly Mock<IRestaurantsRepository> _restaurantsRepositoryMock;
+    private readonly Mock<IMapper> _mapperMock;
+    private readonly Mock<IRestaurantAuthorizationService> _restaurantAuthorizationService;
+
+    private readonly UpdateRestaurantCommandHandler _handler;
+
+    public UpdateRestaurantCommandHandlerTests()
+    {
+        _loggerMock = new Mock<ILogger<UpdateRestaurantCommandHandler>>();
+        _restaurantsRepositoryMock = new Mock<IRestaurantsRepository>();
+        _mapperMock = new Mock<IMapper>();
+        _restaurantAuthorizationService = new Mock<IRestaurantAuthorizationService>();
+        _handler = new UpdateRestaurantCommandHandler(
+            _loggerMock.Object,
+            _mapperMock.Object,
+            _restaurantsRepositoryMock.Object,
+            _restaurantAuthorizationService.Object
+        );
+    }
+
     [Fact]
     public async void Handle_WithValidReqest_ShouldUpdateRestaurant()
     {
         // arrange
-        var loggerMock = new Mock<ILogger<UpdateRestaurantCommandHandler>>();
-        var mapperConfiguration = new MapperConfiguration(cfg =>
-            cfg.AddProfile<RestaurantsProfile>()
-        );
-        var mapper = mapperConfiguration.CreateMapper();
-        var restaurantsRepositoryMock = new Mock<IRestaurantsRepository>();
-
-        var request = new UpdateRestaurantCommand()
+        var restaurantId = 1;
+        var command = new UpdateRestaurantCommand()
         {
-            Id = 1,
+            Id = restaurantId,
             Name = "Name",
             Description = "Desc",
             HasDelivery = true
         };
-        var restaurantToUpdate = new Restaurant();
-        restaurantsRepositoryMock
-            .Setup(r => r.GetByIdAsync(It.IsAny<int>()))
-            .ReturnsAsync(restaurantToUpdate);
-        restaurantsRepositoryMock.Setup(r => r.SaveChangesAsync()).Verifiable();
 
-        var restaurantAuthorizationService = new Mock<IRestaurantAuthorizationService>();
-        restaurantAuthorizationService
+        var restaurantToUpdate = new Restaurant()
+        {
+            Id = restaurantId,
+            Name = "test",
+            Description = "some old description",
+            HasDelivery = false
+        };
+
+        _restaurantsRepositoryMock
+            .Setup(r => r.GetByIdAsync(restaurantId))
+            .ReturnsAsync(restaurantToUpdate);
+
+        _restaurantAuthorizationService
             .Setup(auth => auth.Authorize(restaurantToUpdate, ResourceOperations.Update))
             .Returns(true);
 
-        var commandHandler = new UpdateRestaurantCommandHandler(
-            loggerMock.Object,
-            mapper,
-            restaurantsRepositoryMock.Object,
-            restaurantAuthorizationService.Object
-        );
-
         // act
-        await commandHandler.Handle(request, CancellationToken.None);
+        await _handler.Handle(command, CancellationToken.None);
 
         // asserts
-        restaurantToUpdate.Should().NotBeNull();
-        restaurantToUpdate.Id.Should().Be(request.Id);
-        restaurantToUpdate.Name.Should().Be(request.Name);
-        restaurantToUpdate.Description.Should().Be(request.Description);
-        restaurantToUpdate.HasDelivery.Should().BeTrue();
-        restaurantsRepositoryMock.Verify(r => r.SaveChangesAsync(), Times.Once);
-        //mapper.Verify(m => m.Map(request, restaurantToUpdate), Times.Once);
+        _restaurantsRepositoryMock.Verify(r => r.SaveChangesAsync(), Times.Once);
+        _mapperMock.Verify(m => m.Map(command, restaurantToUpdate), Times.Once);
     }
 
     [Fact]
-    public async void UpdateRestaurant_WhenUpdatedRestaurantNotExist_ThrowNotFoundException()
+    public async void Handle_WhenUpdatedRestaurantNotExist_ThrowNotFoundException()
     {
         // arrange
-        var loggerMock = new Mock<ILogger<UpdateRestaurantCommandHandler>>();
-        var mapperMock = new Mock<IMapper>();
-        var request = new UpdateRestaurantCommand() { Id = 1 };
-        var restaurantsRepositoryMock = new Mock<IRestaurantsRepository>();
-        restaurantsRepositoryMock
-            .Setup(r => r.GetByIdAsync(It.IsAny<int>()))
-            .ReturnsAsync((Restaurant?)null);
-        var restaurantAuthorizationService = new Mock<IRestaurantAuthorizationService>();
+        var restaurantId = 1;
+        var command = new UpdateRestaurantCommand() { Id = restaurantId };
 
-        var commandHandler = new UpdateRestaurantCommandHandler(
-            loggerMock.Object,
-            mapperMock.Object,
-            restaurantsRepositoryMock.Object,
-            restaurantAuthorizationService.Object
-        );
+        _restaurantsRepositoryMock
+            .Setup(r => r.GetByIdAsync(restaurantId))
+            .ReturnsAsync((Restaurant?)null);
 
         // act
-        Func<Task> action = async () =>
-            await commandHandler.Handle(request, CancellationToken.None);
-
+        Func<Task> action = async () => await _handler.Handle(command, CancellationToken.None);
         // asserts
         await action
             .Should()
             .ThrowAsync<NotFoundException>()
-            .WithMessage("Restaurant with id: 1 doesn't exist.");
+            .WithMessage($"Restaurant with id: {restaurantId} doesn't exist.");
     }
 
     [Fact]
-    public async void UpdateRestaurant_WhenUserIsNotAuthorized_ThrowForbiddenException()
+    public async void Handle_WhenUnauthorizedUser_ThrowForbiddenException()
     {
         // arrange
-        var loggerMock = new Mock<ILogger<UpdateRestaurantCommandHandler>>();
-        var mapperMock = new Mock<IMapper>();
-        var request = new UpdateRestaurantCommand() { Id = 1 };
-        var restaurantsRepositoryMock = new Mock<IRestaurantsRepository>();
-        restaurantsRepositoryMock
-            .Setup(r => r.GetByIdAsync(It.IsAny<int>()))
-            .ReturnsAsync(new Restaurant());
-        var restaurantToUpdate = new Restaurant();
+        var restaurantId = 1;
+        var command = new UpdateRestaurantCommand() { Id = restaurantId };
+        Restaurant restaurantToUpdate = new Restaurant() { Id = restaurantId };
 
-        var restaurantAuthorizationService = new Mock<IRestaurantAuthorizationService>();
-        restaurantAuthorizationService
+        _restaurantsRepositoryMock
+            .Setup(r => r.GetByIdAsync(restaurantId))
+            .ReturnsAsync(restaurantToUpdate);
+
+        _restaurantAuthorizationService
             .Setup(auth => auth.Authorize(restaurantToUpdate, ResourceOperations.Update))
             .Returns(false);
-        var commandHandler = new UpdateRestaurantCommandHandler(
-            loggerMock.Object,
-            mapperMock.Object,
-            restaurantsRepositoryMock.Object,
-            restaurantAuthorizationService.Object
-        );
 
         // act
-        Func<Task> action = async () =>
-            await commandHandler.Handle(request, CancellationToken.None);
+        Func<Task> action = async () => await _handler.Handle(command, CancellationToken.None);
 
         // asserts
         await action
             .Should()
             .ThrowAsync<ForbiddenException>()
-            .WithMessage("User not authorized to update restaurant with 1 id");
+            .WithMessage($"User not authorized to update restaurant with {restaurantId} id");
     }
 }
